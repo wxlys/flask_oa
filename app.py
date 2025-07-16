859ea20b5b3af2467aa3d7f7e341a9a7999c9057
wsr <18971461426@163.com>
2025-07-16 14:18:47 +0800

    Changes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from exts import db
 from models import UserModle
 from blueprints.qa import bp as qa_bp
 from blueprints.auth import bp as auth_bp
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 # 从名为 config 的对象导入配置
@@ -11,6 +12,8 @@ app.config.from_object(config)
 
 # 先创建后绑定
 db.init_app(app)
+
+migrate = Migrate(app, db)
 
 # 注册蓝图
 app.register_blueprint(qa_bp)
--- models.py
+++ models.py
@@ -1,4 +1,10 @@
 from exts import db
+from datetime import datetime
 
 class UserModle(db.Model):
-    pass
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False, unique=True)
+    join_time = db.Column(db.DateTime, default=datetime.now)
