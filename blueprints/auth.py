523699ed1ef9e8271f801c77501cf6246636f654
wsr <18971461426@163.com>
2025-07-16 15:20:41 +0800

    导入html前端文件（模板）/静态文件，将html转换为jinja2模板（base父/子模板构造，block head/titie/body）渲染

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -5,9 +5,9 @@ bp = BLUEPRINT = Blueprint('auth', __name__, url_prefix='/auth')
 # /auth/login
 @bp.route('/login')
 def login():
-    pass
+    return render_template('login.html')
 
 
 @bp.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template("register.html")
--- blueprints/qa.py
+++ blueprints/qa.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # 名称--固定写法--前缀
 bp = BLUEPRINT = Blueprint('qa', __name__, url_prefix='/')
@@ -6,5 +6,13 @@ bp = BLUEPRINT = Blueprint('qa', __name__, url_prefix='/')
 # http://127.0.0.1:5000/
 @bp.route('/')
 def index():
+    return render_template('index.html')
+
+@bp.route('/public_question')
+def public_question():
+    return render_template('public_question.html')
+
+@bp.route('/search')
+def search():
     pass
 
